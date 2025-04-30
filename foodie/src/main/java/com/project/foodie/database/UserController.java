ac1ab1c726e98aedf46babcd672735400ccb61d7
Marta <marta.kot191@gmial.com>
2025-04-30 10:33:23 +0200

    Register account third commit

--- foodie/src/main/java/com/project/foodie/database/UserController.java
+++ foodie/src/main/java/com/project/foodie/database/UserController.java
@@ -1,15 +1,15 @@
 package com.project.foodie.database;
 
+import com.project.foodie.administration.RegisterMessage;
 import com.project.foodie.administration.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
+@RequestMapping("/api")
 public class UserController {
 
     private final UserService userService;
@@ -19,11 +19,8 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PutMapping("/users/{id}")      //w bazie danych id jest serizal i możliwe, że nie potrzebujemy id
-    // jak nie bd działać to spróbować zmienić na /users i usunąć @PathVariable final int id
-    public ResponseEntity<User> createUser(@PathVariable final int id, @RequestBody final User user) {
-        user.setId(id);      //jak nie db działać to to też usunąć
-        final User savedUser = userService.createUser(user);
-        return new ResponseEntity<User>(savedUser, HttpStatus.CREATED);
+    @PostMapping("/register")
+    public RegisterMessage register(@RequestBody final User user) {
+        return userService.registerUser(user);
     }
 }
